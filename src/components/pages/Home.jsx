8fa2482f550e8b78461b8335d6a7b2ebf4937a2c
Zahid Safi <zahidsafi956@gmail.com>
2023-05-07 23:25:20 -0600

    made usable for mobile

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="homePage">
-      <h2 class="text-4xl font-extrabold dark:text-white">Payments tool for companies</h2>
-<p class="my-4 text-lg text-gray-500">Start developing with an open-source library of over 450+ UI components, sections, and pages built with the utility classes from Tailwind CSS and designed in Figma.</p>
-<p class="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate critical development work, eliminate toil, and deploy changes with ease.</p>
-
+    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <h2 className="text-4xl font-extrabold text-purple-400">
+        Hello! My name is Zahid Safi
+      </h2>
+      <p className="my-4 text-lg text-purple-400">
+        I am a Software Engineer
+      </p>
     </div>
   );
 };
--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,25 +1,62 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <header>
     <nav className="border-gray-200 bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <ul className="font-medium flex p-0 mt-4">
-          <li className="block py-2 pl-3 pr-4 text-purple-400 hover:bg-gray-700">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="block py-2 pl-3 pr-4 text-purple-400 hover:bg-gray-700">
-            <Link to="/work_experience">Work Experience</Link>
-          </li>
-          <li className="block py-2 pl-3 pr-4 text-purple-400 hover:bg-gray-700">
-            <Link to="/projects">Projects</Link>
-          </li>
-        </ul>
+        <span className="self-center text-2xl font-semibold whitespace-nowrap text-purple-400">
+          ZS
+        </span>
+        <button
+          onClick={toggleMenu}
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 hover:bg-gray-700 focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div
+          className={`${
+            menuOpen ? "block" : "hidden"
+          } w-full md:block md:w-auto`}
+          
+        >
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900">
+            <li className="block py-2 pl-3 pr-4 text-purple-400 hover:bg-gray-700">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="block py-2 pl-3 pr-4 text-purple-400 hover:bg-gray-700">
+              <Link to="/work_experience">Work Experience</Link>
+            </li>
+            <li className="block py-2 pl-3 pr-4 text-purple-400 hover:bg-gray-700">
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
-    </header>
   );
 };
 
